1b8a98231cfd7d600597d793206e5073b7b216de
Marcus Cauê <cauemarcuscauefb@gmail.com>
2023-05-03 09:57:01 -0300

    Gerando interatividade com  NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,19 +3,19 @@ import { Link } from "react-router-dom";
 export default function NavBar() {
   return (
     <nav className="flex flex-row justify-evenly border-b border-border">
-      <Link to="/repositorios">Repositórios</Link>
+      <Link className="link" to="/repositorios">Repositórios</Link>
       <div className="linha-vertical"></div>
 
-      <Link to="/formacao">Formação</Link>
+      <Link className="link" to="/formacao">Formação</Link>
       <div className="linha-vertical"></div>
 
-      <Link to="/perfil">Perfil</Link>
+      <Link className="link" to="/perfil">Perfil</Link>
       <div className="linha-vertical"></div>
 
-      <Link to="/experiencias">Experiências</Link>
+      <Link className="link" to="/experiencias">Experiências</Link>
       <div className="linha-vertical"></div>
 
-      <Link to="/contatos">Contatos</Link>
+      <Link className="link" to="/contatos">Contatos</Link>
     </nav>
   )
 }
