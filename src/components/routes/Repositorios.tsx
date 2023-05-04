607649d45cee9e31157c4a87418847d92ef9a305
Marcus Cauê <cauemarcuscauefb@gmail.com>
2023-05-04 15:01:05 -0300

    Estilizando a seção de repositórios

--- src/components/routes/Repositorios.tsx
+++ src/components/routes/Repositorios.tsx
@@ -23,26 +23,28 @@ export default function Repositorios() {
       {
         repos.map((repo, key) => {
           return (
-            <section key={key} className="">
-              <h1> { repo.name } </h1>
+            <section key={key} className="px-10 py-5">
+              <h1 className="text-emphasis text-xl"> { repo.name } </h1>
 
               <div id="informacoes" className="flex justify-between">
-                <div id="links" className="flex flex-col">
-                  <span> Acesse os arquivos do repositório clicando <a href={ repo.html_url } target="_blank"> aqui </a> </span>
+                <div id="links" className="flex flex-col justify-center gap-5">
+                  <span> Acesse os arquivos do repositório clicando <a href={ repo.html_url } target="_blank" className="text-emphasis destaque"> aqui </a> </span>
                   { 
                     repo.homepage !== "" && repo.homepage !== null ? 
-                      <span> Site em Produção: <Link to={ repo.homepage }> { repo.homepage.substring(8) } </Link> </span>
+                      <span> Site em Produção: <a href={ repo.homepage } target="_blank" className="text-blue-400 destaque"> { repo.homepage.substring(8) } </a> </span>
                     :
                       ''
                   }
                 </div>
-                <div id="outras_informacoes" className="flex flex-col">
+
+                <div id="outras_informacoes" className="flex flex-col gap-5">
                   <span> Data de criação: { repo.created_at } </span>
                   <span> Última atualização: { repo.updated_at } </span>
                   <span> Principal linguagem: { repo.language } </span>
                 </div>
               </div>
 
+              <hr className="mt-5" />
             </section>
           )
         })
